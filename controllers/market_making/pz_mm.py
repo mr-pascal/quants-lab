b0fcc58a57834aa90d5d178177e60f114ca3b918
Pascal Zwikirsch <pascalrene.zwikirsch@gmail.com>
2025-04-03 17:49:14 +0200

    initial setup

---
+++ controllers/market_making/pz_mm.py
@@ -0,0 +1,312 @@
+from decimal import Decimal
+from typing import List
+
+import pandas_ta as ta  # noqa: F401
+from pydantic import Field, validator
+import pandas as pd
+from hummingbot.client.config.config_data_types import ClientFieldData
+from hummingbot.data_feed.candles_feed.data_types import CandlesConfig
+from hummingbot.strategy_v2.controllers.market_making_controller_base import (
+    MarketMakingControllerBase,
+    MarketMakingControllerConfigBase,
+)
+from hummingbot.strategy_v2.executors.position_executor.data_types import (
+    PositionExecutorConfig,
+)
+
+# FIXME: remove Take Profit and Stop Loss from config
+# instead use a "factor" as input for TP and SL. Factored to NATR
+
+
+class PZMMControllerConfig(MarketMakingControllerConfigBase):
+    controller_name = "pz_mm"
+    candles_config: List[CandlesConfig] = []
+    buy_spreads: List[float] = Field(
+        default="1,2,4",
+        client_data=ClientFieldData(
+            is_updatable=True,
+            prompt_on_new=True,
+            prompt=lambda mi: "Enter a comma-separated list of buy spreads (e.g., '0.01, 0.02'):",
+        ),
+    )
+    sell_spreads: List[float] = Field(
+        default="1,2,4",
+        client_data=ClientFieldData(
+            is_updatable=True,
+            prompt_on_new=True,
+            prompt=lambda mi: "Enter a comma-separated list of sell spreads (e.g., '0.01, 0.02'):",
+        ),
+    )
+    candles_connector: str = Field(
+        default=None,
+        client_data=ClientFieldData(
+            prompt_on_new=True,
+            prompt=lambda mi: "Enter the connector for the candles data, leave empty to use the same exchange as the connector: ",
+        ),
+    )
+    candles_trading_pair: str = Field(
+        default=None,
+        client_data=ClientFieldData(
+            prompt_on_new=True,
+            prompt=lambda mi: "Enter the trading pair for the candles data, leave empty to use the same trading pair as the connector: ",
+        ),
+    )
+    interval: str = Field(
+        default="5m",
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the candle interval (e.g., 1m, 5m, 1h, 1d): ",
+            prompt_on_new=False,
+        ),
+    )
+
+    hma_very_slow: int = Field(
+        default=50,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the HMA very slow length: ",
+            prompt_on_new=True,
+        ),
+    )
+    hma_slow: int = Field(
+        default=20,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the HMA slow length: ", prompt_on_new=True
+        ),
+    )
+    hma_fast: int = Field(
+        default=10,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the HMA fast length: ", prompt_on_new=True
+        ),
+    )
+    rsi_length: int = Field(
+        default=9,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the stoch RSI length: ", prompt_on_new=True
+        ),
+    )
+
+    stoch_rsi_smoothing: int = Field(
+        default=3,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the Stoch RSI smoothing length: ",
+            prompt_on_new=True,
+        ),
+    )
+    stoch_rsi_length: int = Field(
+        default=9,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the Stoch RSI length: ",
+            prompt_on_new=True,
+        ),
+    )
+    natr_length: int = Field(
+        default=14,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the NATR length: ", prompt_on_new=True
+        ),
+    )
+    tp_natr_factor: Decimal = Field(
+        default=0.5,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the TP NATR Factor : ", prompt_on_new=True
+        ),
+    )
+    sl_natr_factor: Decimal = Field(
+        default=0.5,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the SL NATR Factor : ", prompt_on_new=True
+        ),
+    )
+
+
+    @validator("candles_connector", pre=True, always=True)
+    def set_candles_connector(cls, v, values):
+        if v is None or v == "":
+            return values.get("connector_name")
+        return v
+
+    @validator("candles_trading_pair", pre=True, always=True)
+    def set_candles_trading_pair(cls, v, values):
+        if v is None or v == "":
+            return values.get("trading_pair")
+        return v
+
+
+class PZMMController(MarketMakingControllerBase):
+    """
+    This is a pz version of the PMM controller.It uses the MACD to shift the mid-price and the NATR
+    to make the spreads pz. It also uses the Triple Barrier Strategy to manage the risk.
+    """
+
+    def __init__(self, config: PZMMControllerConfig, *args, **kwargs):
+        self.config = config
+        self.max_records = (
+            max(
+                config.hma_very_slow,
+                config.hma_slow,
+                config.hma_fast,
+                config.rsi_length,
+                config.natr_length,
+            )
+            + 100
+        )
+        if len(self.config.candles_config) == 0:
+            self.config.candles_config = [
+                CandlesConfig(
+                    connector=config.candles_connector,
+                    trading_pair=config.candles_trading_pair,
+                    interval=config.interval,
+                    max_records=self.max_records,
+                )
+            ]
+        super().__init__(config, *args, **kwargs)
+
+    def get_price_multiplier(
+        self,
+        candles: pd.DataFrame,
+        natr_length: int,
+        rsi_length: int,
+        stoch_rsi_length: int,
+        stoch_rsi_smoothing: int,
+        hma_very_slow: int,
+        hma_slow: int,
+        hma_fast: int,
+    ) -> tuple[pd.Series, pd.Series]:
+        natr = (
+            ta.natr(
+                candles["high"],
+                candles["low"],
+                candles["close"],
+                length=natr_length,
+            )
+            / 100
+        )
+        rsi = ta.rsi(candles["close"], length=rsi_length)
+        rsi_wma = ta.wma(rsi, length=rsi_length)
+
+        stochrsi = ta.stochrsi(
+            candles["close"],
+            length=stoch_rsi_length,
+            rsi_length=stoch_rsi_length,
+            k=stoch_rsi_smoothing,
+            d=stoch_rsi_smoothing,
+            mamode="sma",
+            append=False,
+        )
+
+        k = stochrsi[
+            f"STOCHRSIk_{stoch_rsi_length}_{stoch_rsi_length}_{stoch_rsi_smoothing}_{stoch_rsi_smoothing}"
+        ]
+        d = stochrsi[
+            f"STOCHRSId_{stoch_rsi_length}_{stoch_rsi_length}_{stoch_rsi_smoothing}_{stoch_rsi_smoothing}"
+        ]
+
+        hma_very_slow_output = ta.hma(
+            candles["close"],
+            length=hma_very_slow,
+            offset=0,
+        )
+        hma_slow_output = ta.hma(
+            candles["close"],
+            length=hma_slow,
+            offset=0,
+        )
+        hma_fast_output = ta.hma(
+            candles["close"],
+            length=hma_fast,
+            offset=0,
+        )
+        hma_very_slow_signal = candles["close"] - hma_very_slow_output
+        hma_very_slow_signal = hma_very_slow_signal.apply(
+            lambda x: 1 if x > 0 else -1
+        )
+        hma_slow_signal = candles["close"] - hma_slow_output
+        hma_slow_signal = hma_slow_signal.apply(lambda x: 1 if x > 0 else -1)
+        hma_fast_signal = candles["close"] - hma_fast_output
+        hma_fast_signal = hma_fast_signal.apply(lambda x: 1 if x > 0 else -1)
+
+        k_over_d = k / d
+        k_over_d_signal = k_over_d.apply(lambda x: 1 if x > 0 else -1)
+        max_price_shift = natr * 0.8
+        srsi_over_signal = d.apply(
+            lambda x: -1 if x > 80 else (1 if x < 20 else 0)
+        )
+
+        rsi_wma_signal = (rsi - rsi_wma).apply(lambda x: 1 if x > 0 else -1)
+        rsi_over_signal = rsi.apply(
+            lambda x: -1 if x > 70 else (1 if x < 30 else 0)
+        )
+
+        signal_multipliers = [
+            # HMA
+            hma_very_slow_signal,
+            hma_slow_signal,
+            hma_fast_signal,
+            # SRSI
+            k_over_d_signal,
+            srsi_over_signal,
+            # RSI
+            rsi_wma_signal,
+            rsi_over_signal,
+        ]
+        # Create even weights to the signals
+        signal_multiplier = sum(
+            signal / len(signal_multipliers) for signal in signal_multipliers
+        )
+        price_multiplier = signal_multiplier * max_price_shift
+        return price_multiplier, natr
+
+    async def update_processed_data(self):
+        candles = self.market_data_provider.get_candles_df(
+            connector_name=self.config.candles_connector,
+            trading_pair=self.config.candles_trading_pair,
+            interval=self.config.interval,
+            max_records=self.max_records,
+        )
+
+        price_multiplier, natr = self.get_price_multiplier(
+            candles=candles,
+            natr_length=self.config.natr_length,
+            rsi_length=self.config.rsi_length,
+            stoch_rsi_length=self.config.stoch_rsi_length,
+            stoch_rsi_smoothing=self.config.stoch_rsi_smoothing,
+            hma_very_slow=self.config.hma_very_slow,
+            hma_slow=self.config.hma_slow,
+            hma_fast=self.config.hma_fast,
+        )
+        price_multiplier = price_multiplier.iloc[-1]
+        candles["spread_multiplier"] = natr
+        candles["reference_price"] = candles["close"] * (1 + price_multiplier)
+        self.processed_data = {
+            "reference_price": Decimal(candles["reference_price"].iloc[-1]),
+            "spread_multiplier": Decimal(candles["spread_multiplier"].iloc[-1]),
+            "features": candles,
+        }
+
+    def get_executor_config(
+        self, level_id: str, price: Decimal, amount: Decimal
+    ):
+        trade_type = self.get_trade_type_from_level_id(level_id)
+        # TODO: this "get_executor_config" happens all the time, so we can dynamically change the config!
+        # triple_barrier_config is built dyncamically on every access (getter)
+
+        # I want to make the take profit and stop loss dynamic, so it's based on the
+        # NATR of the underlying asset, so it can be used for high and low volatility assets
+
+        # TP = 1x NATR
+        self.take_profit = self.config.tp_natr_factor * self.processed_data["spread_multiplier"]
+
+        # SL = 1x NATR
+        self.stop_loss = self.config.sl_natr_factor * self.processed_data["spread_multiplier"]
+
+        return PositionExecutorConfig(
+            timestamp=self.market_data_provider.time(),
+            level_id=level_id,
+            connector_name=self.config.connector_name,
+            trading_pair=self.config.trading_pair,
+            entry_price=price,
+            amount=amount,
+            triple_barrier_config=self.config.triple_barrier_config,
+            leverage=self.config.leverage,
+            side=trade_type,
+        )
