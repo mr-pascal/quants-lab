8628bbb6433916a0c3f2ef4a27de4173b76bfea0
Pascal Zwikirsch <pascalrene.zwikirsch@gmail.com>
2025-04-20 11:28:46 +0200

    .

--- _CUSTOM/check_db_connection.py
+++ _CUSTOM/check_db_connection.py
@@ -9,8 +9,25 @@ conn = psycopg2.connect(
 )
 
 cur = conn.cursor()
-cur.execute("SELECT * FROM studies;")
-print(cur.fetchall())
+
+cur.execute(
+    """
+    SELECT
+    t.study_id,
+    s.study_name,
+    count(*)
+    FROM trials t
+    JOIN studies s ON s.study_id = t.study_id
+    GROUP BY t.study_id, s.study_name
+    ORDER BY t.study_id DESC
+    """
+)
+rows = cur.fetchall()
+
+
+rows = sorted(rows, key = lambda x:x[0])
+for row in rows:
+    print(row)
 
 cur.close()
 conn.close()
